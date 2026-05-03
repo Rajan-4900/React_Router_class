993e3d84215f5cf5ca89dad75bedf9db5ec5cd12
Rajan-91480 <rajan91480@gmail.com>
2026-05-03 23:10:00 +0530

    this new code is used to setup the router to run in the browser and the route -> self closing tag used to locate the path in search link

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
-import {} from 'react-router-dom'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Home from './Pages/Home'
+import About from './Pages/About'
+import Contact from './Pages/Contact'
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+    <Router>
+      <Routes>
+        {/* this "<Route /> Use to add the path to the search link in the browser" */}
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
+
+      </Routes>
+    </Router>
+    </>
   )
 }
 
