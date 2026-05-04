e2a4945977bfd9ff76e7a0e068f6eb66729fd2a3
Rajan-91480 <rajan91480@gmail.com>
2026-05-04 22:23:02 +0530

    link and desings added tot the new components

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -4,7 +4,11 @@ import { Link } from 'react-router-dom'
 const Navbar = () => {
   return (
     <>
-    <Link to={'/'}></Link>
+    <div style={{backgroundColor:'white', padding:'10px'}}>
+    <Link style={{margin:'10px', }} to={'/'}>Home</Link>
+    <Link style={{margin:'10px', }} to={'/about'}>About</Link>
+    <Link style={{margin:'10px', }} to={'/contact'}>Contact Us</Link>
+    </div>
     </>
   )
 }
