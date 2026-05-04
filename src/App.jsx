283be4da0454f374d749921809d81873190b0141
Rajan-91480 <rajan91480@gmail.com>
2026-05-04 22:15:24 +0530

    link is created to navigate the link path in browser

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Home from './Pages/Home'
 import About from './Pages/About'
 import Contact from './Pages/Contact'
+import Navbar from './Components/Navbar'
 
 const App = () => {
   return (
     <>
     <Router>
+      <Navbar />
       <Routes>
         {/* this "<Route /> Use to add the path to the search link in the browser" */}
         <Route path='/' element={<Home />} />
