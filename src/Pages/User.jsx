dfd4be5ad93f4b4c50ac56be608ad4209c5b229d
Rajan-91480 <rajan91480@gmail.com>
2026-05-08 22:59:03 +0530

    new user added in the user files with maping function

--- src/Pages/User.jsx
+++ src/Pages/User.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 
 const User = () => {
-
+    const user = [
+        { id:1, name:"Rajan", gmail:"rajan@gmail.com"},
+        { id:2, name:"Raj", gmail:"raj@gmail.com"},
+        { id:3, name:"Raju", gmail:"raju@gmail.com"},
+        { id:4, name:"Rajesh", gmail:"rajesh@gmail.com"}
+    ]
   return (
-
+    <>
+    {user.map((data)=><div key={data.id}>
+        <h1></h1>
+    </div>)}
+    </>
   )
 }
 
