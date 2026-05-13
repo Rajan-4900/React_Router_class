036f341a32adab98fcd58e4105e75de1fee757f1
Rajan-91480 <rajan91480@gmail.com>
2026-05-13 22:24:29 +0530

    we have created a useParams in the dynamicUser file

--- src/Pages/DynamicUser.jsx
+++ src/Pages/DynamicUser.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 
 const DynamicUser = () => {
+  // console.log(useParams())
+
+  // useParams() is a hook that returns an object of key/value pairs of the dynamic params from the current URL that were matched by the <Route path>.
+  // In this case, it will return an object with the key 'id' and the value will be whatever is in the URL after '/user/'.
+  const {id} = useParams()
   return (
-    <div>DynamicUser</div>
+    <div>
+      <h1>ID : {id}</h1>
+    </div>
   )
 }
 
