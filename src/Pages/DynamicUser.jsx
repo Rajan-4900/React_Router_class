cb8bf040f7340554a0e443dc3b8d957a99afd46a
Rajan-91480 <rajan91480@gmail.com>
2026-05-13 22:34:52 +0530

    this new code is for displaying the values in the screen instead of in the console using filter and specificUser[0]

--- src/Pages/DynamicUser.jsx
+++ src/Pages/DynamicUser.jsx
@@ -6,10 +6,22 @@ const DynamicUser = () => {
 
   // useParams() is a hook that returns an object of key/value pairs of the dynamic params from the current URL that were matched by the <Route path>.
   // In this case, it will return an object with the key 'id' and the value will be whatever is in the URL after '/user/'.
-  const {id} = useParams()
+  const { id } = useParams()
+  const user = [
+    { id: 1, name: "Rajan", gmail: "rajan@gmail.com", age: 20 },
+    { id: 2, name: "Raj", gmail: "raj@gmail.com", age: 20 },
+    { id: 3, name: "Raju", gmail: "raju@gmail.com", age: 20 },
+    { id: 4, name: "Rajesh", gmail: "rajesh@gmail.com", age: 20 }
+  ];
+  const specificUser = user.filter(data => data.id == id) // this will filter the user array and return the specific user object that matches the id from the URL. We use '==' instead of '===' because the id from the URL is a string and the id in the user array is a number.
+  console.log(specificUser) // this will return an array with the specific user object that matches the id from the URL.
   return (
+    // this will displays the values in the screen instead of in the console. We use specificUser[0] because filter() returns an array and we want to access the first element of that array which is the specific user object.
     <div>
-      <h1>ID : {id}</h1>
+      <h1>ID : {specificUser[0].id}</h1>
+      <h1>Name : {specificUser[0].name}</h1>
+      <h1>Gmail : {specificUser[0].gmail}</h1>
+      <h1>Age : {specificUser[0].age}</h1>
     </div>
   )
 }
